8625c215ba05ef9b556ce9b107642bdf5baf6dfb
Bartosz Szczygiel <eziosoft@gmail.com>
2022-09-12 09:53:48 +0200

    cleanup

--- app/src/main/java/com/eziosoft/parisinnumbers/presentation/DetailsScreen.kt
+++ app/src/main/java/com/eziosoft/parisinnumbers/presentation/DetailsScreen.kt
@@ -3,7 +3,6 @@ package com.eziosoft.parisinnumbers.presentation
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
@@ -12,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat.startActivity
 import org.koin.androidx.compose.getViewModel
 import org.koin.core.parameter.parametersOf
@@ -20,34 +20,45 @@ import org.koin.core.parameter.parametersOf
 fun DetailsScreen(movieId: String?) {
     val viewModel = getViewModel<DetailsScreenViewModel> { parametersOf(movieId) }
     val screenState = viewModel.screenStateFlow.collectAsState()
-
     val context = LocalContext.current
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        Column() {
-            Text("Title: " + screenState.value.movieTitle)
-            Text("Address: " + screenState.value.address)
-            Text("Year: " + screenState.value.year)
-            Text("Start date: " + screenState.value.startDate)
-            Text("EndDate: " + screenState.value.endDate)
-            Text("Producer: " + screenState.value.producer)
-            Text("Realisation: " + screenState.value.realisation)
-            Text("Type: " + screenState.value.type)
+    Content(screenState.value, context)
+}
 
-            Button(onClick = {
-                openInGoogleMaps(
-                    context,
-                    screenState.value.lat,
-                    screenState.value.lon
-                )
-            }) {
-                Text(text = "Open in Google Maps")
-            }
+@Composable
+private fun Content(
+    screenState: ScreenState,
+    context: Context?
+) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text("Title: " + screenState.movieTitle)
+        Text("Address: " + screenState.address)
+        Text("Year: " + screenState.year)
+        Text("Start date: " + screenState.startDate)
+        Text("EndDate: " + screenState.endDate)
+        Text("Producer: " + screenState.producer)
+        Text("Realisation: " + screenState.realisation)
+        Text("Type: " + screenState.type)
+
+        Button(onClick = {
+            openInGoogleMaps(
+                context!!,
+                screenState.lat,
+                screenState.lon
+            )
+        }) {
+            Text(text = "Open in Google Maps")
         }
     }
 }
 
-fun openInGoogleMaps(context: Context, lat: Double, lon: Double) {
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    Content(screenState = ScreenState(), context = null)
+}
+
+private fun openInGoogleMaps(context: Context, lat: Double, lon: Double) {
     val url = "http://maps.google.com/maps?q=$lat,$lon"
     val intent = Intent(Intent.ACTION_VIEW)
     intent.data = Uri.parse(url)
--- app/src/main/java/com/eziosoft/parisinnumbers/presentation/ListScreen.kt
+++ app/src/main/java/com/eziosoft/parisinnumbers/presentation/ListScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,14 +17,16 @@ import org.koin.androidx.compose.getViewModel
 @Composable
 fun ListScreen(onItemClick: (movieTitle: String) -> Unit) {
     val viewModel = getViewModel<ListScreenViewModel>()
-
     val movies = viewModel.getMovies().collectAsLazyPagingItems()
+    val listState: LazyListState = rememberLazyListState()
+
     Box(
         modifier = Modifier
             .fillMaxSize()
     ) {
         LazyColumn(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize(),
+            state = listState
         ) {
             items(movies) { item ->
                 item?.record?.let { record ->
