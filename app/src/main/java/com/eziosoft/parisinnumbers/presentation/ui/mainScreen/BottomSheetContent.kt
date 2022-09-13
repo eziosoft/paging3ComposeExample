286eb3eb3c5c9ce5692ce5343495fdfbc2603278
Bartosz Szczygiel <eziosoft@gmail.com>
2022-09-13 13:31:57 +0200

    colors

--- app/src/main/java/com/eziosoft/parisinnumbers/presentation/ui/mainScreen/BottomSheetContent.kt
+++ app/src/main/java/com/eziosoft/parisinnumbers/presentation/ui/mainScreen/BottomSheetContent.kt
@@ -2,16 +2,21 @@ package com.eziosoft.parisinnumbers.presentation.ui.mainScreen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.eziosoft.parisinnumbers.presentation.ui.theme.PrimaryLight
 
 @Composable
 fun BottomSheetContent() {
     Box(
-        modifier = Modifier.background(Color.LightGray)
+        modifier = Modifier
+            .clip(RoundedCornerShape(topStart = 15.dp, topEnd = 15.dp))
+            .background(PrimaryLight)
     ) {
         Text("Sheet Content", fontSize = 80.sp)
     }
--- app/src/main/java/com/eziosoft/parisinnumbers/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/eziosoft/parisinnumbers/presentation/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.eziosoft.parisinnumbers.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Accent = Color(0xFF9E9E9E)
+val Primary = Color(0xFF607D8B)
+val PrimaryDark = Color(0xFF455A64)
+val PrimaryLight = Color(0xFFCFD8DC)
