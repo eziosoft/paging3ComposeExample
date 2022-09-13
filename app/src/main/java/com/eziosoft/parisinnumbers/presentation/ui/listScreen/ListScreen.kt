895ff564562151798af10e662db7b0393770106f
Bartosz Szczygiel <eziosoft@gmail.com>
2022-09-13 13:22:46 +0200

    make list nicer

--- app/src/main/java/com/eziosoft/parisinnumbers/presentation/ui/listScreen/ListScreen.kt
+++ app/src/main/java/com/eziosoft/parisinnumbers/presentation/ui/listScreen/ListScreen.kt
@@ -1,17 +1,22 @@
 package com.eziosoft.parisinnumbers.presentation.ui.listScreen
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
+import com.eziosoft.parisinnumbers.data.remote.models.records.RecordX
 import org.koin.androidx.compose.getViewModel
 
 @Composable
@@ -28,17 +33,27 @@ fun ListScreen() {
         ) {
             items(movies) { item ->
                 item?.record?.let { record ->
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-                                viewModel.navigateToDetails(recordId = record.id)
-                            }
-                    ) {
-                        Text(text = record.fields.nom_tournage)
-                    }
+                    ListItem(viewModel, record)
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun ListItem(
+    viewModel: ListScreenViewModel,
+    record: RecordX
+) {
+    Card(modifier = Modifier
+        .clickable {
+            viewModel.navigateToDetails(recordId = record.id)
+        }
+        .fillMaxWidth()
+        .padding(4.dp), elevation = 5.dp) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            Text(text = record.fields.nom_tournage, fontWeight = FontWeight.Bold)
+            Text(text = record.fields.annee_tournage)
         }
     }
 }
