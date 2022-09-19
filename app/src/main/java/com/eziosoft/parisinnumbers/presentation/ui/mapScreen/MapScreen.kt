2748d5655b26fbcc3b5ddda2341e48f1be36baab
Bartosz Szczygiel <eziosoft@gmail.com>
2022-09-19 14:39:40 +0200

    marker clustering optimisation

--- app/src/main/java/com/eziosoft/parisinnumbers/presentation/ui/mapScreen/MapScreen.kt
+++ app/src/main/java/com/eziosoft/parisinnumbers/presentation/ui/mapScreen/MapScreen.kt
@@ -1,12 +1,16 @@
 package com.eziosoft.parisinnumbers.presentation.ui.mapScreen
 
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.maps.android.clustering.ClusterManager
 import com.google.maps.android.compose.*
 import com.google.maps.android.heatmaps.HeatmapTileProvider
@@ -53,14 +57,21 @@ fun MapScreen() {
             properties = properties,
             cameraPositionState = cameraPositionState
         ) {
-
+            var clusterManager by remember { mutableStateOf<ClusterManager<ClusterMarker>?>(null) }
             MapEffect(key1 = markers) { googleMap ->
-                val clusterManager = ClusterManager<ClusterMarker>(context, googleMap)
-                clusterManager.setAnimation(false)
-                googleMap.setOnCameraIdleListener(clusterManager)
-                googleMap.setOnMarkerClickListener(clusterManager)
+                clusterManager = ClusterManager<ClusterMarker>(context, googleMap)
+                clusterManager?.let { clusterManager ->
+                    clusterManager.setAnimation(false)
+                    googleMap.setOnCameraIdleListener(clusterManager)
+                    googleMap.setOnMarkerClickListener(clusterManager)
+                    clusterManager.addItems(markers.map { ClusterMarker(it.position, "", "") })
+                }
+            }
 
-                clusterManager.addItems(markers.map { ClusterMarker(it.position, "", "") })
+            LaunchedEffect(key1 = cameraPositionState.isMoving) {
+                if (!cameraPositionState.isMoving) {
+                    clusterManager?.onCameraIdle()
+                }
             }
 
             heatmapTileProvider?.let {
